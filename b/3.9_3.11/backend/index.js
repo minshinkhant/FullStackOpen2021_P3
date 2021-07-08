bea4ac0ac5c6bb029030792befca3cf7daebbc8f
Min Shin Khant <minshinkhant7@gmail.com>
2021-07-08 14:09:27 -0400

    adding b/3.9_3.11

---
+++ b/3.9_3.11/backend/index.js
@@ -0,0 +1,103 @@
+const { response } = require('express')
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+
+const app = express()
+
+
+app.use(express.json())
+app.use(cors())
+
+app.use(morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      JSON.stringify(req.body)
+    ].join(' ')
+  }))
+
+let contacts = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+//get all the contacts
+app.get('/api/persons', (request, response) => {
+    response.json(contacts)
+})
+
+//get info of the page
+app.get('/info', (request,response) => {
+    const info = `<p> Phonebook has info for ${contacts.length} people </p>`
+    response.send(info + new Date())
+})
+
+//get a specific contact
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const contact = contacts.find(contact => contact.id === id)
+    if (contact) {
+        response.json(contact)
+    } else {
+        response.status(404).end()
+    }
+})
+
+//delete the contact
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    contacts = contacts.filter(contact => contact.id !== id)
+    response.status(204).end()
+})
+
+//post new contact
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+    else if (contacts.find(contact => contact.name.toLowerCase() === body.name.toLowerCase())){
+        return response.status(204).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const contact = {
+        id: Math.floor(Math.random()*(100 - contacts.length)+contacts.length),
+        name: body.name,
+        number: body.number
+    }
+    contacts = contacts.concat(contact)
+    response.json(contact)
+})
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
