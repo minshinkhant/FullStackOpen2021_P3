b3d97a74c8cc7445fd9e1f6f08226f03b3a922ed
min <min@mins-MacBook-Air.local>
2021-07-15 21:26:33 -0400

    adding c/3.13_3.14

---
+++ c/3.13_3.14/README.md
@@ -0,0 +1,14 @@
+# Exercises 3.13.-3.14.
+The following exercises are pretty straightforward, but if your frontend stops working with the backend, then finding and fixing the bugs can be quite interesting.
+
+## 3.13: Phonebook database, step1
+Change the fetching of all phonebook entries so that the data is fetched from the database.
+
+Verify that the frontend works after the changes have been made.
+
+In the following exercises, write all Mongoose-specific code into its own module, just like we did in the chapter Database configuration into its own module.
+
+## 3.14: Phonebook database, step2
+Change the backend so that new numbers are saved to the database. Verify that your frontend still works after the changes.
+
+At this point, you can choose to simply allow users to create all phonebook entries. At this stage, the phonebook can have multiple entries for a person with the same name.
---
+++ c/3.13_3.14/backend/index.js
@@ -0,0 +1,79 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const Contact = require('./models/contact')
+
+const app = express()
+
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
+app.use(morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      JSON.stringify(req.body)
+    ].join(' ')
+  }))
+
+//get all the contacts
+app.get('/api/persons', (request, response) => {
+    Contact.find({}).then(contacts => {
+        response.json(contacts)
+    })
+})
+
+//get info of the page
+app.get('/info', (request,response) => {
+    const info = `<p> Phonebook has info for ${contacts.length} people </p>`
+    response.send(info + new Date())
+})
+
+//get a specific contact
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const contact = contacts.find(contact => contact.id === id)
+    if (contact) {
+        response.json(contact)
+    } else {
+        response.status(404).end()
+    }
+})
+
+//delete the contact
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    contacts = contacts.filter(contact => contact.id !== id)
+    response.json(contacts)
+})
+
+//post new contact
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+    const contact = new Contact({
+        name: body.name,
+        number: body.number
+    })
+
+    contact.save().then(Contact.find({}).then(contacts => {
+        response.json(contacts)
+    })
+    )
+})
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
