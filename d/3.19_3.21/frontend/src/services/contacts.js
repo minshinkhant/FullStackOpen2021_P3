d740ac4d9b9175a94c62881a0a46ffd927bfcaa8
min <min@mins-MacBook-Air.local>
2021-07-27 16:35:33 -0400

    adding d/3.19_3.21

---
+++ d/3.19_3.21/frontend/src/services/contacts.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+const baseUrl = '/api/persons';
+
+const getAll = () => {
+    const request = axios.get(baseUrl);
+    return request.then(response => response.data);
+}
+
+const create = (newObject) => {
+    const request = axios.post(baseUrl, newObject);
+    return request.then(response => response.data);
+}
+
+const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject);
+    return request.then(response => response.data);
+}
+
+const deleteContact = (id, name) => () => {
+    if (window.confirm(`Do you want to delete ${name}`)) {
+        const request = axios.delete(`${baseUrl}/${id}`);
+        const newContacts = request.then(response => response.data);
+        window.location.reload();
+        return newContacts;
+    }
+    return false;
+
+}
+
+const contactService = {
+    getAll: getAll,
+    create: create,
+    update: update,
+    deleteContact: deleteContact
+}
+
+export default contactService;
